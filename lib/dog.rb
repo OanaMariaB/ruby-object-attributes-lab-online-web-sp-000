81c5ed42e2a49036a82598b0d10c53447ad38c8f
Oana Berentan <oana.berentan@yahoo.com>
2019-05-18 19:24:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog
-  def initialize (name)
-    @name = name
-  end
+  #def initialize (name)
+  #  @name = name
+#  end
 
-  def name
+  def name(name)
+    @name = name
     "{@name}"
   end
 
